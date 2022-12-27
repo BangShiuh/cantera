bf60c2327c9ae820547606a1d751c3a7913107e1
bangshiuh <bangshiuh.chen@gmail.com>
2022-12-27 05:19:50 -0500

    [thermo] set energy levels and cross section to NAN when node has no key for method setParameters

--- src/kinetics/ElectronCollisionPlasmaRate.cpp
+++ src/kinetics/ElectronCollisionPlasmaRate.cpp
@@ -65,12 +65,14 @@ void ElectronCollisionPlasmaRate::setParameters(const AnyMap& node, const UnitSt
         m_energyLevels = Eigen::Map<const Eigen::ArrayXd>(levels.data(), levels.size());
     } else {
         m_energyLevels = NAN;
+        return;
     }
     if (node.hasKey("cross-section")) {
         vector_fp crossSection = node["cross-section"].asVector<double>();
         m_crossSection = Eigen::Map<const Eigen::ArrayXd>(crossSection.data(), crossSection.size());
     } else {
         m_crossSection = NAN;
+        return;
     }
     if (m_energyLevels.size() != m_crossSection.size()) {
         throw CanteraError("ElectronCollisionPlasmaRate::setParameters",
