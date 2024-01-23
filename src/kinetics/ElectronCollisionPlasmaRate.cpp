4e2930060b0a03d33b55c59c14e33c8c09675887
Bang-Shiuh Chen <bangshiuh.chen@gmail.com>
2024-01-22 22:24:05 -0500

    [reaction] make forward and reverse electron collision rate calculation consistent

--- src/kinetics/ElectronCollisionPlasmaRate.cpp
+++ src/kinetics/ElectronCollisionPlasmaRate.cpp
@@ -101,8 +101,8 @@ double ElectronCollisionPlasmaRate::evalFromStruct(const ElectronCollisionPlasma
     );
 
     // unit in kmol/m3/s
-    return 0.5 * pow(2.0 * ElectronCharge / ElectronMass, 0.5) * Avogadro *
-           simpson(distribution.cwiseProduct(cs_array), eps.pow(2.0));
+    return pow(2.0 * ElectronCharge / ElectronMass, 0.5) * Avogadro *
+           simpson(eps.cwiseProduct(distribution.cwiseProduct(cs_array)), eps);
 }
 
 void ElectronCollisionPlasmaRate::modifyRateConstants(
